69b5b817f29b34105cb7cb5ac2cef2a151839248
helloimhai <hai.nguyen@ninjavan.co>
2020-08-08 18:51:13 +0800

    Set up cors

--- main/app.js
+++ main/app.js
@@ -3,11 +3,18 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./controllers/index");
 var tellRouter = require("./controllers/tells");
 
 var app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 
 app.use(logger("dev"));
 app.use(express.json());
